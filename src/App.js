517d688b0b1075d0703638d80cb558355fe3aaf7
Rajan Agarwal <rajan.ag005@gmail.com>
2021-11-27 00:17:06 -0500

    initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
+import Main from './pages/Main';
+import Visualize from './pages/Visualize';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Main />} />
+      <Route path="/data" element={<Visualize />} />
+    </Routes>
+  </BrowserRouter>
   );
 }
 
 export default App;
+ 
---
+++ src/pages/Main.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "../App.css";
+
+export default function Main() {
+  return (
+    <div className="App">
+        <section>
+            <h1>Project Theseus</h1>
+            <h3>Access To Water Is Not a Privelage</h3>
+            <p>Literally just throw all research and ideas on here</p>
+        </section>
+    </div>
+  );
+}
